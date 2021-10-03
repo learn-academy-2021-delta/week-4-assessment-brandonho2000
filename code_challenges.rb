e7c6a76a2162154944eeca8411ca05a31bbd80c5
brandonho2000 <bradho099@gmail.com>
2021-10-03 17:07:29 -0500

    finnished assessment, ready for review.

--- code_challenges.rb
+++ code_challenges.rb
@@ -5,21 +5,43 @@
 
 num1 = 7
 # Expected output: '7 is odd'
+
+if num1 %2 == 0
+    p "#{num1} is even"
+else
+    p "#{num1} is odd"
+end
 num2 = 42
 # Expected output: '42 is even'
+if num2 %2 == 0
+    p "#{num2} is even"
+else
+    p "#{num2} is odd"
+end
 num3 = 221
 # Expected output: '221 is odd'
-
+if num1 %2 == 0
+    p "#{num3} is even"
+else
+    p "#{num3} is odd"
+end
 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
 album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
+p album1.delete "aeiou" 
+
 album2 = 'Sgt Pepper'
 # Expected output: 'Sgt Pppr'
+p album2.delete "aeiou"
+
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
+p album3.downcase.delete"aeiou"
+
+
 
 
 
@@ -27,7 +49,22 @@ album3 = 'Abbey Road'
 
 is_palindrome1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
+if is_palindrome1.downcase == is_palindrome1.downcase.reverse
+    p "#{is_palindrome1} is a palindrome"
+else
+    p "#{is_palindrome1} is not a palindrome"
+end
 is_palindrome2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
+if is_palindrome2 == is_palindrome2.reverse
+    p "#{is_palindrome2} is a palindrome"
+else
+    p "#{is_palindrome2} is not a palindrome"
+end
 is_palindrome3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+if is_palindrome3.downcase == is_palindrome3.downcase.reverse
+    p "#{is_palindrome3} is a palindrome"
+else
+    p "#{is_palindrome3} is not a palindrome"
+end
